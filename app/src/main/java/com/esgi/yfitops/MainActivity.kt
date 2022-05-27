6508324956d4d99d98c3dbbbea1073848177aac9
Ancelotow <ancelotow@gmail.com>
2022-05-27 16:59:01 +0200

    .feat: ephemeral button of navigation

--- app/src/main/java/com/esgi/yfitops/MainActivity.kt
+++ app/src/main/java/com/esgi/yfitops/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.esgi.yfitops
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.albums).setOnClickListener {
+            val i = Intent(this, AlbumActivity::class.java)
+            startActivity(i)
+        }
+        findViewById<Button>(R.id.classement).setOnClickListener {
+            val i = Intent(this, RankingActivity::class.java)
+            startActivity(i)
+        }
+        findViewById<Button>(R.id.artists).setOnClickListener {
+            val i = Intent(this, ArtistActivity::class.java)
+            startActivity(i)
+        }
     }
+
 }
