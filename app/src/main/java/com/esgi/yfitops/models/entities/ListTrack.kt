1af1b5bc364fb42e897551d95e92ce80436f0432
ANCELOT Owen <ancelotow@gmail.com>
2022-07-17 11:40:10 +0200

    .refactor: review entity Track

---
+++ app/src/main/java/com/esgi/yfitops/models/entities/ListTrack.kt
@@ -0,0 +1,4 @@
+package com.esgi.yfitops.models.entities
+
+class ListTrack {
+}
--- app/src/main/java/com/esgi/yfitops/models/entities/Track.kt
+++ app/src/main/java/com/esgi/yfitops/models/entities/Track.kt
@@ -3,41 +3,29 @@ package com.esgi.yfitops.models.entities
 import com.esgi.yfitops.models.services.ApiConnection
 import com.esgi.yfitops.models.services.ApiService
 import com.google.gson.annotations.SerializedName
-import org.json.JSONObject
 import retrofit2.await
 
-class Track(trackJson: JSONObject) {
+class Track(
 
     @SerializedName("idTrack")
-    var id: Int
+    var id: Int,
 
     @SerializedName("idAlbum")
-    var idAlbum: Int
+    var idAlbum: Int,
 
     @SerializedName("idArtist")
-    var idArtist: Int
+    var idArtist: Int,
 
     @SerializedName("strTrack")
-    var title: String
+    var title: String,
 
     @SerializedName("strArtist")
-    var artist: String
+    var artist: String,
 
     @SerializedName("strTrackThumb")
     var thumb: String
 
-    init {
-        this.id = trackJson.getInt("idTrack")
-        this.idAlbum = trackJson.getInt("idAlbum")
-        this.idArtist = trackJson.getInt("idArtist")
-        this.title = trackJson.getString("strTrack")
-        this.artist = trackJson.getString("strArtist")
-        this.thumb = trackJson.getString("strTrackThumb")
-        if (this.thumb == "null" || this.thumb == "") {
-            this.thumb =
-                "https://us.123rf.com/450wm/soloviivka/soloviivka1606/soloviivka160600001/59688426-music-note-vecteur-icône-blanc-sur-fond-noir.jpg"
-        }
-    }
+) {
 
     companion object Service {
 
